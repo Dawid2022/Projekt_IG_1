3c55a5b018e3b463e0e4ae88ad20d49ad58cf58f
Dawid2022 <dawidpaska24@gmail.com>
2024-05-03 13:16:20 +0200

    dodanie flag BL->1992 i BL->2000

--- Projekt_infa_1.py
+++ Projekt_infa_1.py
@@ -1,5 +1,5 @@
 import sys 
-from math import sin, cos, sqrt, atan, atan2, degrees, radians, pi
+from math import sin, cos, sqrt, atan, atan2, degrees, radians, pi, tan
 import numpy as np
 
 o = object()
@@ -28,8 +28,10 @@ class Transformacje:
         else:
             raise NotImplementedError(f"{model} model not implemented")
         self.flat = (self.a - self.b) / self.a
-        self.ecc = sqrt(2 * self.flat - self.flat ** 2) # eccentricity  WGS84:0.0818191910428 
-        self.ecc2 = (2 * self.flat - self.flat ** 2) # eccentricity**2
+        self.ecc = sqrt(2 * self.flat - self.flat ** 2) # first eccentricity  WGS84:0.0818191910428 
+        self.ecc2 = (2 * self.flat - self.flat ** 2) # first eccentricity**2
+        self.eccp = sqrt(self.ecc2 / (1 - self.ecc2)) # second eccentricity
+        self.eccp2 = (self.ecc2 / (1 - self.ecc2)) # second eccentricity**2
 
 
     
@@ -74,7 +76,9 @@ class Transformacje:
             return f"{lat[0]:02d}:{lat[1]:02d}:{lat[2]:.2f}", f"{lon[0]:02d}:{lon[1]:02d}:{lon[2]:.2f}", f"{h:.3f}"
         else:
             raise NotImplementedError(f"{output} - output format not defined")
+    
             
+    
     def plh2xyz(self, phi, lam, h):
         phi = radians(phi)
         lam = radians(lam)
@@ -87,7 +91,11 @@ class Transformacje:
         return x,y,z
     
     
-    def lambda0_l(lam):
+    
+    def pl2000(self,phi,lam):
+        phi = radians(phi)
+        lam = radians(lam)
+        
         
         if lam <= (11*pi)/120:
             numer = 5
@@ -101,51 +109,62 @@ class Transformacje:
         elif lam > (15*pi)/120:
             numer = 8
             lam0 = np.radians(24)
-            
-        return(lam0,numer)
+        
+        N = self.a / sqrt(1 - self.ecc2 * sin(phi)**2)
+        A0 = 1 - self.ecc2/4 - (3*self.ecc2**2) /64 - (5*self.ecc2**3) /256
+        A2 = 3/8 * (self.ecc2 + (self.ecc2**2) /4 + (15*self.ecc2**3) /128)
+        A4 = 15/256 * (self.ecc2**2 + (3*self.ecc2**3) /4)
+        A6 = (35*self.ecc2**3) /3072
+        sigma = self.a*(A0*phi - A2*sin(2*phi) + A4*sin(4*phi) - A6*sin(6*phi))    
+        
+        dlam = lam - lam0
+        t = tan(phi)
+        eta2 = self.eccp2 * (cos(phi)**2)
+        
+        xGK = sigma + ((dlam**2 * N*sin(phi)*cos(phi))/2) * (1 + ((dlam**2 /12) * cos(phi)**2) * (5 - t**2 + 9*eta2 + 4*eta2**2)
+                + ((dlam**4/360) * (cos(phi)**4)) * (61 - 58*t**2 + t**4 + 270*eta2 - 330*eta2*t**2))
+        yGK = (dlam*N*cos(phi)) * (1 + ((dlam**2 /6) * cos(phi)**2) * (1 - t**2 + eta2) 
+                + ((dlam**4 /120) * cos(phi)**4) * (5 - 18*t**2 + t**4 + 14*eta2 - 58*eta2*t**2))
+        
+        x2000 = xGK * 0.999923
+        y2000 = yGK * 0.999923 + numer*1000000 + 500000
     
-    def sigma(self, phi):
+        return(x2000,y2000)
+    
+
+
+    def pl92(self,phi,lam):
+        phi = radians(phi)
+        lam = radians(lam)
+        
+        lam0 = np.radians(19)
+        N = self.a / sqrt(1 - self.ecc2 * sin(phi)**2)
         
         A0 = 1 - self.ecc2/4 - (3*self.ecc2**2) /64 - (5*self.ecc2**3) /256
         A2 = 3/8 * (self.ecc2 + (self.ecc2**2) /4 + (15*self.ecc2**3) /128)
         A4 = 15/256 * (self.ecc2**2 + (3*self.ecc2**3) /4)
         A6 = (35*self.ecc2**3) /3072
-        
         sigma = self.a*(A0*phi - A2*sin(2*phi) + A4*sin(4*phi) - A6*sin(6*phi))    
-        
-        return(sigma)
-    
-    
-    def Np(self, phi):
-        
-        N = self.a / sqrt(1 - self.ecc2 * sin(phi)**2)
-        
-        return(N)
 
-
-    def f1(self,xGK): #!!!
+        dlam = lam - lam0
+        t = tan(phi)
+        eta2 = self.eccp2 * (cos(phi)**2)
         
-        A0 = 1 - self.ecc2/4 - (3*self.ecc2**2) /64 - (5*self.ecc2**3) /256
-        fj = xGK/(self.a*A0)
+        xGK = sigma + ((dlam**2 * N*sin(phi)*cos(phi))/2) * (1 + ((dlam**2 /12) * cos(phi)**2) * (5 - t**2 + 9*eta2 + 4*eta2**2)
+                + ((dlam**4/360) * (cos(phi)**4)) * (61 - 58*t**2 + t**4 + 270*eta2 - 330*eta2*t**2))
+        yGK = (dlam*N*cos(phi)) * (1 + ((dlam**2 /6) * cos(phi)**2) * (1 - t**2 + eta2) 
+                + ((dlam**4 /120) * cos(phi)**4) * (5 - 18*t**2 + t**4 + 14*eta2 - 58*eta2*t**2))
         
-        while True:
-            
-            fi = fj
-            sig = self.sigma(phi)
-            
-            fj = fi + (xGK - sig)/(self.a*A0)
-            
-            if np.abs(fj - fi) <(0.000001 / 206265):
-                break
+        x1992 = xGK * 0.9993 - 5300000
+        y1992 = yGK * 0.9993 + 500000
         
-        return(fj)
+        return(x1992,y1992)
 
 
 
 if __name__ == "__main__":
     # utworzenie obiektu
     geo = Transformacje(model = "wgs84")
-    print("To jest argv: ", sys.argv)
     # dane XYZ geocentryczne
     # X = 3664940.500; Y = 1409153.590; Z = 5009571.170
     # phi, lam, h = geo.xyz2plh(X, Y, Z)
@@ -155,17 +174,15 @@ if __name__ == "__main__":
     
 
 
-    input_file_path = sys.argv
+    input_file_path = sys.argv[-1]
     
     if '--xyz2plh' in sys.argv and '--plh2xyz' in sys.argv:
         print('możesz podać tylko jedną flagę')
     elif '--xyz2plh' in sys.argv:
         
-    
-        with open(input_file_path[2],'r') as f:
+        with open(input_file_path,'r') as f:
             dane = f.readlines()
             dane = dane[4:]
-            
             
             plh = []
             for d in dane:
@@ -185,10 +202,9 @@ if __name__ == "__main__":
     
     elif '--plh2xyz' in sys.argv:
     
-        with open(input_file_path[2],'r') as f:
+        with open(input_file_path,'r') as f:
             dane = f.readlines()
             dane = dane[1:]
-            
             
             xyz = []
             for d in dane:
@@ -204,3 +220,48 @@ if __name__ == "__main__":
             for coords in xyz:
                 coords_xyz_line = ','.join([str(coord) for coord in coords])
                 f.write(coords_xyz_line + '\n')
+    
+    
+    elif '--pl2000' in sys.argv:
+        
+        with open(input_file_path,'r') as f:
+            dane = f.readlines()
+            dane = dane[1:]
+        
+            xy = []
+            for d in dane:
+                
+                d = d.strip()
+                phi_str,lam_str,h_str = d.split(',')
+                phi,lam = (float(phi_str),float(lam_str))
+                x,y = geo.pl2000(phi,lam)
+                xy.append([x,y])
+            
+        with open('wyniki_pl2000.txt','w') as f:
+            f.write('x2000[m], y2000[m] \n')
+            for coords in xy:
+                coords_xy_line = ','.join([str(coord) for coord in coords])
+                f.write(coords_xy_line + '\n')
+
+                
+    elif '--pl1992' in sys.argv:
+        
+        with open(input_file_path,'r') as f:
+            dane = f.readlines()
+            dane = dane[1:]
+        
+            xy = []
+            for d in dane:
+                
+                d = d.strip()
+                phi_str,lam_str,h_str = d.split(',')
+                phi,lam = (float(phi_str),float(lam_str))
+                x,y = geo.pl92(phi,lam)
+                xy.append([x,y])
+            
+        with open('wyniki_pl1992.txt','w') as f:
+            f.write('x1992[m], y1992[m] \n')
+            for coords in xy:
+                coords_xy_line = ','.join([str(coord) for coord in coords])
+                f.write(coords_xy_line + '\n')
+        
